78092a28464cab7da3056791460eebd2decdb6f7
Harsh Jaiswal <136101726+harshjaiswalprgm@users.noreply.github.com>
2023-09-23 01:05:00 +0530

    update

--- function.cpp
+++ function.cpp
@@ -1,8 +1,8 @@
 /*#include <iostream>
 using namespace std;
- 
+
 void myfunction();  // Function declaration
-void myfunction()  
+void myfunction()
 {
     cout << "It's time to break man.." << endl; //function body//(defination)
 }
@@ -24,7 +24,91 @@ int main() {
 
 void myFunction() {
   cout << "I just got executed!";
-}            // Error
+}                                       // Error
+
+
+#include<iostream>              //   C++ Default Parameters
+using namespace std;
+void myFunction(string fname){
+    cout<<fname<<"Refsnes"<<endl;
+
+}
+int main(){
+    myFunction("Harsh");
+    myFunction("Abhi");
+    myFunction("Ayush");
+    return 0;
+
+}
+
+#include <iostream>                 //C++ Multiple Parameters//
+using namespace std;
+void myfunction(string country = "Africa")
+{
+    cout << country << endl;
+}
+
+int main()
+{
+myfunction("sweden");
+myfunction("India");
+myfunction();
+myfunction("USA");
+    return 0;
+}
+
+#include <iostream>
+using namespace std;
+void myfunction(string fname, int age)
+{
+    cout << fname<< "Refsnes." << age << "years old." << endl;
+}
+
+int main()
+{
+    myfunction("Harsh", 21);
+    myfunction("Abhi", 23);
+    myfunction("Rishav", 18);
+
+    return 0;
+}
+
+#include<iostream>
+using namespace std;
+int myfunction(int x){
+    return 5+x;
+
+}
+int main(){
+
+    cout<<myfunction(3);
+    return 0;
+}
+
+// This example returns the sum of a function with two parameters:
+
+#include <iostream>
+using namespace std;
+int myFunction(int x, int y)
+{
+    return x + y;
+}
+
+int main()
+{
+    cout<< myFunction(5, 3);
+    return 0;
+}
 
 */
-#
+#include<iostream>
+using namespace std;
+int myFunction(int x, int y) {
+  return x + y;
+}
+
+int main() {
+  int z = myFunction(5, 3);
+  cout << z;
+  return 0;
+}
