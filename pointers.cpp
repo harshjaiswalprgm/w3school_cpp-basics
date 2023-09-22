3ee61e558df0b37b8b91aaa48295b4df026af16a
Harsh Jaiswal <136101726+harshjaiswalprgm@users.noreply.github.com>
2023-09-22 23:08:12 +0530

    update

--- pointers.cpp
+++ pointers.cpp
@@ -5,8 +5,27 @@ using namespace std;
 int main()
 {
     string foood = "chapati";
+    string* ptr =&foood;   //string* mystring;-Preferred
     cout << foood << endl;
     cout << &foood << endl;
+    cout<< ptr<<endl;
+    cout<<*ptr<<endl;   //Dereference Operator//
     return 0;
 }
+
 */
+
+#include<iostream>
+using namespace std;
+int main(){
+string food = "Rice";
+string* ptr =&food;
+cout<<food<<endl;
+cout<<*ptr<<endl;
+cout<<&food<<endl;
+*ptr = pulse;
+cout<<*ptr<<endl;
+
+
+    return 0;
+}
