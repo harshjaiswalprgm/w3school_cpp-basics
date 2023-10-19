5b9b6a31295d0fde543e2c54394f7c4a6fd7a3d3
Harsh Jaiswal <136101726+harshjaiswalprgm@users.noreply.github.com>
2023-10-19 10:49:17 +0530

    update

---
+++ arrayloop.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int array[5] = {10, 0, 20, 0, 30};
+    for (int i = 0; i < 5; i++)
+    {
+        cout << array[i] << endl;
+    }
+//4++
+    return 0;
+}
