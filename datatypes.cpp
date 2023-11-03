adf454c0cc531b3b842b0b0114938a076c215c74
Harsh Jaiswal <136101726+harshjaiswalprgm@users.noreply.github.com>
2023-11-03 23:10:38 +0530

    update

---
+++ datatypes.cpp
@@ -0,0 +1,30 @@
+// Data Types***
+
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    // int myNum = 35;
+    // char myLetter = 'A';
+    // bool myBoolean = true;
+    // float myFlooatNum = 2.22;
+    // double myDoubleNum = 2.23;
+    // string myText = "Hello";
+
+    int myNum = 5;             // Integer (whole number)
+    float myFloatNum = 5.99;   // Floating point number
+    double myDoubleNum = 9.98; // Floating point number
+    char myLetter = 'D';       // Character
+    bool myBoolean = true;     // Boolean
+    string myText = "Hello";   // String
+
+    cout << "int: " << myNum << endl;
+    cout << "char: " << myLetter << endl;
+    cout << "bool: " << myBoolean << endl;
+    cout << "float: " << myFloatNum << endl;
+    cout << "double: " << myDoubleNum << endl;
+    cout << "string: " << myText << endl;
+
+    return 0;
+}
