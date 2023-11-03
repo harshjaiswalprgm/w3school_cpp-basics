b738405a672d7bf276ed3e6ca14785dd3633b149
Harsh Jaiswal <136101726+harshjaiswalprgm@users.noreply.github.com>
2023-11-03 23:13:00 +0530

    update

---
+++ sepdatatypes.cpp
@@ -0,0 +1,43 @@
+
+// C++ Boolean Data Types***
+
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    bool isCodingFun = true;
+    bool isFishTasty = false;
+
+    cout << isCodingFun << endl;
+    cout << isFishTasty << endl;
+
+    return 0;
+}
+
+// Chracter Types ***
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    char myGrade = 'B';
+    cout << myGrade << endl;
+    char a = 65, b = 66, c = 67;
+    cout << a << endl;
+    cout << b << endl;
+    cout << c << endl;
+    return 0;
+}
+
+//   C++ String Data Types***
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+
+    string greeting = "Fuck u Garima and Sakshi yr tum too hayyeeeee";
+    cout << greeting << endl;
+    return 0;
+}
