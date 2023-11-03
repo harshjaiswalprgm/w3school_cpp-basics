59299daa3105b8547f4872987911f889b5491ea7
Harsh Jaiswal <136101726+harshjaiswalprgm@users.noreply.github.com>
2023-11-03 23:08:26 +0530

    update

--- C++.cpp
+++ C++.cpp
@@ -53,56 +53,8 @@ int main() {
   return 0;
 }
 
-                                Creating a Simple Calculator***
+ 
 
-
-
-#include<iostream>
-using namespace std;
-
-int main(){
-int x, y;
-int sum;
-cout << "Press a number: ";
-cin >> x;
-cout << "Type another number: ";
-cin >> y;
-sum = x + y;
-cout << "Sum is: " << sum;
-return 0;
-
-}
-
-                                      Data Types***
-
-#include <iostream>
-using namespace std;
-int main()
-{
-
-    int myNum = 35;
-    char myLetter = 'A';
-    bool myBoolean = true;
-    float myFlooatNum = 2.22;
-     double myDoubleNum = 2.23;
-     string myText = "Hello";
-
-   /* int myNum = 5;             // Integer (whole number)
-    float myFloatNum = 5.99;   // Floating point number
-    double myDoubleNum = 9.98; // Floating point number
-    char myLetter = 'D';       // Character
-    bool myBoolean = true;     // Boolean
-    string myText = "Hello";   // String
-
-cout << "int: " << myNum << endl;
-cout << "char: " << myLetter << endl;
-cout << "bool: " << myBoolean << endl;
-cout << "float: " << myFlooatNum << endl;
-cout << "double: " << myDoubleNum << endl;
-cout << "string: " << myText << endl;
-
-return 0;
-}
 
 
                                     Numeric Types***
---
+++ simplecalculatorsum.cpp
@@ -0,0 +1,17 @@
+//Creating a Simple Calculator***
+
+#include <iostream>
+    using namespace std;
+
+int main()
+{
+    int x, y;
+    int sum;
+    cout << "Press a number: ";
+    cin >> x;
+    cout << "Type another number: ";
+    cin >> y;
+    sum = x + y;
+    cout << "Sum is: " << sum;
+    return 0;
+}
