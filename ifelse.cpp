fe94f3243af6d2fbdb946591e09d54c9f31654dc
Harsh Jaiswal <136101726+harshjaiswalprgm@users.noreply.github.com>
2024-02-03 00:59:14 +0530

    update

---
+++ ifelse.cpp
@@ -0,0 +1,13 @@
+// 
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  int a = 20;
+  string result = (a < 18) ? "Good day." : "Good evening.";
+  cout << result;
+  return 0;
+}
+	
---
+++ referencee.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+#include<string>
+int main(){
+    // string food ="pizza";
+    // string &meal =food;
+    // cout<<food<<endl;
+    // cout<<meal<<endl;
+    // cout<<&food<<endl;
+    // cout<<&meal<<endl;
+
+
+
+int i=4;
+cin>>"enter the no.";
+
+for(i= 0; i<4; i++){
+    cout<<"i is runs 4 times"<<endl;
+}
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
